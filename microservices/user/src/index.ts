4c6548c3c7bb923e3cc870a7379fa03ae7420f24
Ferhat Korkmaz <ferhatkorkmaz@email.com>
2024-09-16 03:03:08 +0300

    update microservices

--- microservices/user/src/index.ts
+++ microservices/user/src/index.ts
@@ -1,8 +1,14 @@
 import { Express } from "express";
+import { register } from "@ferhatkorkmaz11/infrastack-interview-fs-fk-20240916";
+register({
+  endpoint: "http://localhost:4317",
+  instruments: ["express", "http"],
+});
 
 const app: Express = require("express")();
-
+register;
 app.get("/", (req, res) => {
+  console.log(`received request from host:${req.headers.host}`);
   res.send("Hello World from User service!");
 });
 
---
+++ microservices/user/src/type.d.ts
@@ -0,0 +1,1 @@
+declare module "@ferhatkorkmaz11/infrastack-interview-fs-fk-20240916";
