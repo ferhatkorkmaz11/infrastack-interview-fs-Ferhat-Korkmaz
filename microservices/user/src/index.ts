67c1a8a140a3acb2716042d866c60f743b238874
Ferhat Korkmaz <ferhatkorkmaz@email.com>
2024-09-17 23:53:46 +0300

    update usage in user service

--- microservices/user/src/index.ts
+++ microservices/user/src/index.ts
@@ -1,17 +1,37 @@
 import { Express } from "express";
-import { register } from "@ferhatkorkmaz11/infrastack-interview-fs-fk-20240916";
-register({
-  endpoint: "http://localhost:4317",
-  instruments: ["express", "http"],
-});
+import {
+  register,
+  log,
+} from "@ferhatkorkmaz11/infrastack-interview-fs-fk-20240916";
 
-const app: Express = require("express")();
-register;
-app.get("/", (req, res) => {
-  console.log(`received request from host:${req.headers.host}`);
-  res.send("Hello World from User service!");
-});
+async function startServer() {
+  const endpoint =
+    process.env.OTEL_EXPORTER_OTLP_ENDPOINT ?? "http://localhost:4317";
+  await register({
+    endpoint: endpoint,
+    instruments: ["express", "http"],
+  });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
-});
+  const app: Express = require("express")();
+
+  app.get("/", async (req, res) => {
+    try {
+      log.info(`Received request from host: ${req.headers.host}`);
+      const response = await fetch("http://localhost:8082");
+      res.send("Hello World from User service!");
+    } catch (error) {
+      log.error("Error handling request:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  app.get("/404", (req, res) => {
+    res.status(404).send("Not Found");
+  });
+
+  app.listen(8080, () => {
+    log.info("User service is running on port 8080");
+  });
+}
+
+startServer();
